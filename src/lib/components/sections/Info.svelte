10ac135f9e6599e59a43e69a8d5fec757d5b3e9d
Luke Hagar <lukeslakemail@gmail.com>
2024-05-24 14:17:17 -0500

    Formatted info for no scroll

--- src/lib/components/sections/Info.svelte
+++ src/lib/components/sections/Info.svelte
@@ -4,53 +4,53 @@
 	import { openApiStore } from '$lib';
 </script>
 
-<form
-	class="container mx-auto border-token rounded-container-token bg-surface-backdrop-token px-6 py-4 space-y-2"
->
-	<label class=" space-y-1">
-		<span class="text-sm">Title (required)</span>
-		<input
-			class="input text-sm"
-			name="title"
-			placeholder="Sample API"
-			type="text"
-			bind:value={$openApiStore.info.title}
-			required
-		/>
-	</label>
-	<label class=" space-y-1">
-		<span class="text-sm"> Description (optional) </span>
-		<textarea
-			class="textarea"
-			name="description"
-			placeholder="Optional multiline or single-line description. Supports Markdown."
-			bind:value={$openApiStore.info.description}
-		/>
-	</label>
-	<label class=" space-y-1">
-		<span class="text-sm">Version (required)</span>
-		<input
-			class="input text-sm"
-			name="version"
-			placeholder="0.1.0"
-			type="text"
-			bind:value={$openApiStore.info.version}
-			required
-		/>
-	</label>
-	<label class=" space-y-1">
-		<span class="text-sm">Terms of Service (optional)</span>
-		<input
-			class="input text-sm"
-			name="termsOfService"
-			placeholder="https://example.com/terms"
-			type="url"
-			bind:value={$openApiStore.info.termsOfService}
-		/>
-	</label>
-	<div class="border-token rounded-container-token space-y-2 p-4">
+<form class="space-y-2">
+	<div class="border-token rounded-container-token bg-surface-backdrop-token space-y-1 p-4">
+		<label class="space-y-1">
+			<span class="text-sm">Title (required)</span>
+			<input
+				class="input text-sm"
+				name="title"
+				placeholder="Sample API"
+				type="text"
+				bind:value={$openApiStore.info.title}
+				required
+			/>
+		</label>
+		<label class="space-y-1">
+			<span class="text-sm"> Description (optional) </span>
+			<textarea
+				class="textarea"
+				name="description"
+				placeholder="Optional multiline or single-line description. Supports Markdown."
+				bind:value={$openApiStore.info.description}
+			/>
+		</label>
+		<label class="space-y-1">
+			<span class="text-sm">Version (required)</span>
+			<input
+				class="input text-sm"
+				name="version"
+				placeholder="0.1.0"
+				type="text"
+				bind:value={$openApiStore.info.version}
+				required
+			/>
+		</label>
+		<label class="space-y-1">
+			<span class="text-sm">Terms of Service (optional)</span>
+			<input
+				class="input text-sm"
+				name="termsOfService"
+				placeholder="https://example.com/terms"
+				type="url"
+				bind:value={$openApiStore.info.termsOfService}
+			/>
+		</label>
+	</div>
+	<div class="border-token rounded-container-token bg-surface-backdrop-token space-y-1 p-4">
 		<h4 class="h4">Contact Information</h4>
-		<label class=" space-y-1">
+		<label class="space-y-1">
 			<span class="text-sm">Name (optional)</span>
 			<input
 				class="input text-sm"
@@ -60,7 +60,7 @@
 				bind:value={$openApiStore.info.contact.name}
 			/>
 		</label>
-		<label class=" space-y-1">
+		<label class="space-y-1">
 			<span class="text-sm">Email (optional)</span>
 			<input
 				class="input text-sm"
@@ -70,7 +70,7 @@
 				bind:value={$openApiStore.info.contact.email}
 			/>
 		</label>
-		<label class=" space-y-1">
+		<label class="space-y-1">
 			<span class="text-sm">URL (optional)</span>
 			<input
 				class="input text-sm"
