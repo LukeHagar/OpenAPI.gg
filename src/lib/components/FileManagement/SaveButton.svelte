1f39913a2146e8b75fcbe9d637209eaf4df9f89e
Luke Hagar <lukeslakemail@gmail.com>
2024-05-29 11:20:17 -0700

    Added save button

---
+++ src/lib/components/FileManagement/SaveButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { openApiStore } from '$lib';
+	import { db, selectedSpec } from '$lib/db';
+
+	function onSave(e: Event): void {
+		console.log('Save button clicked');
+		if (!$selectedSpec) {
+			$selectedSpec = {
+				name: 'New OpenAPI Spec',
+				spec: $openApiStore
+			};
+		}
+		console.log($selectedSpec);
+		db.apiSpecs.put($selectedSpec, $selectedSpec.id);
+	}
+</script>
+
+<button class="btn variant-ghost-success" on:click={onSave}> Save </button>
