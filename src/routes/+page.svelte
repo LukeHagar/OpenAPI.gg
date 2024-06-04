c72ed736b367aea7356e521e611cba4c51176495
Luke Hagar <lukeslakemail@gmail.com>
2024-06-03 21:32:09 -0500

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,16 +25,14 @@
 	<table class="table">
 		<thead>
 			<tr>
-				<th class="text-left">ID</th>
 				<th class="text-left">Name</th>
-				<th class="text-left">Actions</th>
+				<th class="text-center">Actions</th>
 			</tr>
 		</thead>
 		<tbody>
 			{#if $apiSpecs}
 				{#each $apiSpecs as spec (spec.id)}
 					<tr>
-						<td>{spec.id}</td>
 						<td>{spec.name}</td>
 						<td>
 							<LoadButton {spec} />
@@ -53,32 +51,10 @@
 			placeholder="Enter the name for the API Spec"
 		/>
 	{/if}
+
 	<SaveButton />
 	<SaveNewButton />
 	<CreateNewButton />
 	<Upload />
 	<DeleteAllButton />
 </div>
-<!-- <div class="w-full h-full flex flex-col items-center justify-center grow">
-	<h1 class="h1">
-		<span
-			class="bg-gradient-to-br from-blue-500 to-cyan-300 bg-clip-text text-transparent box-decoration-clone"
-		>
-			Design.
-		</span>
-	</h1>
-	<h1 class="h1">
-		<span
-			class="bg-gradient-to-br from-red-500 to-yellow-500 bg-clip-text text-transparent box-decoration-clone"
-		>
-			Build.
-		</span>
-	</h1>
-	<h1 class="h1">
-		<span
-			class="bg-gradient-to-br from-pink-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
-		>
-			Deploy.
-		</span>
-	</h1>
-</div> -->
