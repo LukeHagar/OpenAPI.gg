0b3842ce704df68cb44a79a499520e5605b5c02c
Luke Hagar <lukeslakemail@gmail.com>
2024-07-05 00:39:36 +0000

    added method page

--- src/routes/paths/[...pathName]/z/[method]/+page.svelte
+++ src/routes/paths/[...pathName]/z/[method]/+page.svelte
@@ -8,7 +8,7 @@
 	class="border-token border-surface-500 space-y-4 px-6 py-4 rounded-container-token variant-glass-surface"
 >
 	<h3 class="h3 font-mono">
-		Path: {$page.params.pathName}#{$page.params.method}
+		Path: {$page.params.pathName} Method: {$page.params.method.toUpperCase()}
 	</h3>
 	<hr />
 </div>
